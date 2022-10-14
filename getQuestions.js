98209cb322b10e4e1fb92809c11d2d1721ebead6
Bee <xronsredstone@gmail.com>
2022-10-14 17:14:50 -0400

    that didn't work so i just got rid of it, lol

--- getQuestions.js
+++ getQuestions.js
@@ -1,4 +1,4 @@
-const createQuestionMakerClass = require('./lib/QuestionMaker')
+const createQuestionMakerClass = require('./utils/questionBuilder')
 const questions = ["What is the name of your app?","What is this app for?","How do you install it? ","How do you use it?","How do you report issues?","Did you run any tests? How do you run them?","How can people support this project?","What is your github username? ","What is your email?"];
 const names = ["name","description","install","usage","report","tests","support","username","email"];
 var projectQuestions = createQuestionMakerClass(questions, names)
--- utils/arrayMaker/arrayMaker.js
+++ utils/arrayMaker/arrayMaker.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(err, data) {
     if (err) throw err;
     var input = data.split('\n');
-    input.forEach(string => string.trim());
     const wQuotes = input.map(input => `"${input}"`);
     const finalArray = `[${wQuotes}]`;
     fs.writeFile('array.txt', `${finalArray}`, (err) => err ? console.error(err) : console.log('Created in array.txt'));
