79a8b4fc2b9619859bfca7c1449a3c373e823929
Bee <xronsredstone@gmail.com>
2022-10-14 16:14:37 -0400

    fixed arrayMaker and added our arrays, spent hours making something that saves minutes.....

--- utils/arrayMaker/arrayMaker.js
+++ utils/arrayMaker/arrayMaker.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 fs.readFile('input.txt', 'utf8', function(err, data) {
     if (err) throw err;
-    var licenseChoices = data.split('\r\n')
+    var licenseChoices = data.split('\n')
     licenseChoices.forEach(string => {
         string.trim()
     })
